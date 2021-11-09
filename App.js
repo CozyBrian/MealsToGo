e84bae8aad61222a5299a4b54d9f4f50e910a886
CozyBrian <dragonbrian33@gmail.com>
2021-11-09 14:12:11 +0000

    Added search bar and restaurant screen components

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
-
-const isAndroid = Platform.OS === "android";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, StatusBar } from "react-native";
+import { RestaurantScreen } from "./src/features/restaurants/screens/restaurant.screen";
 
 export default function App() {
   return (
     <>
-    <ExpoStatusBar style="auto" />
-    <SafeAreaView style={styles.SAV}>
-        <View style={styles.searchView}>
-          <Text style={styles.searchText}>Search</Text>
-        </View>
-        <View style={styles.mainView}>
-          <Text style={styles.mainText}>Main</Text>
-        </View>
-    </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+      <RestaurantScreen />
     </>
   );
 }
@@ -32,15 +24,12 @@ const styles = StyleSheet.create({
   searchView: {
     padding: 14,
     justifyContent: "center",
-    backgroundColor: "green",
   },
   mainView: {
     flex: 1,
     padding: 14,
     backgroundColor: "blue",
   },
-  searchText: {
-  },
-  mainText: {
-  }
+  searchText: {},
+  mainText: {},
 });
