214d1db39bcc197e8e9c047ab7672f07f2c6503a
CozyBrian <dragonbrian33@gmail.com>
2021-11-09 15:23:24 +0000

    Removed unused stylesheet

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, StatusBar } from "react-native";
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurant.screen";
 
 export default function App() {
@@ -11,25 +10,3 @@ export default function App() {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  SAV: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  container: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  searchView: {
-    padding: 14,
-    justifyContent: "center",
-  },
-  mainView: {
-    flex: 1,
-    padding: 14,
-    backgroundColor: "blue",
-  },
-  searchText: {},
-  mainText: {},
-});
