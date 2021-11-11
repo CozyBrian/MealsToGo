c84147ca1e86537b478b31a78f2c10b3df1da89d
CozyBrian <dragonbrian33@gmail.com>
2021-11-11 14:22:17 +0000

    Completed Card...Added rating and icons

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,12 +1,15 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
-import { Text } from "react-native";
-import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+import { Text, Image, View } from "react-native";
+import styled from "styled-components/native";
+import { Card, Title, Paragraph } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Havila",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
@@ -16,12 +19,52 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     isClosedTemp = true,
   } = restaurant;
 
+  const Titlee = styled(Title)`
+    font-family: ${(props) => props.theme.fonts.body};
+  `;
+
+  const Row = styled.View`
+    flex-direction: row;
+    align-items: center;
+  `;
+
+  const Right = styled.View`
+    flex: 1;
+    flex-direction: row;
+    justify-content: flex-end;
+  `;
+
+  const Spacer = styled.View`
+    padding-left: 8px;
+  `;
+
+  const P = styled(Paragraph)`
+    font-family: ${(props) => props.theme.fonts.heading};
+  `;
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
-    <Card>
+    <Card elevation={5}>
       <Card.Cover source={{ url: photos[0] }} />
       <Card.Content>
-        <Title>{name}</Title>
-        <Paragraph>{address}</Paragraph>
+        <Titlee>{name}</Titlee>
+        <Row>
+          {ratingArray.map(() => (
+            <SvgXml xml={star} width={20} height={20} />
+          ))}
+          <Right>
+            {isClosedTemp && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORAILY
+              </Text>
+            )}
+            <Spacer />
+            {isOpenedNow ? <SvgXml xml={open} width={20} height={20} /> : null}
+            <Spacer />
+            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+          </Right>
+        </Row>
+        <P>{address}</P>
       </Card.Content>
     </Card>
   );
