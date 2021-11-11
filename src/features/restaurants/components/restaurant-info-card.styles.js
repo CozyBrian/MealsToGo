434fa47e5d6330bc9cf9ad060f186cf89f48fe53
CozyBrian <dragonbrian33@gmail.com>
2021-11-11 18:28:38 +0000

    Styles used in Restaurant-Info-Card

---
+++ src/features/restaurants/components/restaurant-info-card.styles.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { Card, Title, Paragraph } from "react-native-paper";
+
+export const Titlee = styled(Title)`
+  font-family: ${(props) => props.theme.fonts.body};
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Right = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+export const RedText = styled.Text`
+  color: red;
+  font-size: 12px;
+`;
+
+export const Spacer = styled.View`
+  padding-left: 8px;
+`;
+
+export const Icon = styled.Image`
+  width: 15px;
+  height: 15px;
+`;
+
+export const P = styled(Paragraph)`
+  font-family: ${(props) => props.theme.fonts.heading};
+`;
