f62ed2a24363c1c488c67453b3c0b7d3666bffff
CozyBrian <dragonbrian33@gmail.com>
2021-11-11 14:23:25 +0000

    Applied Themeing

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { ThemeProvider } from "styled-components/native";
+
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurant.screen";
+import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ExpoStatusBar style="auto" />
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantScreen />
+      </ThemeProvider>
     </>
   );
 }
