18af3771d147ce8220d2879397260dc119136333
CozyBrian <dragonbrian33@gmail.com>
2021-11-12 16:48:24 +0000

    Implimented BottomTab from React Navigation

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { Text } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SafeArea } from "./src/components/utility/safe-area";
 
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import {
@@ -10,6 +14,30 @@ import {
 
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurant.screen";
 import { theme } from "./src/infrastructure/theme";
+
+const Tab = createBottomTabNavigator();
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+
+const Map = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+);
+
+function MyTabs() {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Restaurant" component={RestaurantScreen} />
+      <Tab.Screen name="Map" component={Map} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -27,7 +55,9 @@ export default function App() {
     <>
       <ExpoStatusBar style="auto" />
       <ThemeProvider theme={theme}>
-        <RestaurantScreen />
+        <NavigationContainer>
+          <MyTabs />
+        </NavigationContainer>
       </ThemeProvider>
     </>
   );
