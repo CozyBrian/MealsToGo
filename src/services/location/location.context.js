2b9d2a7e516adafbb74dd00e865aaec86b04adff
CozyBrian <dragonbrian33@gmail.com>
2021-11-14 02:29:17 +0000

    a location context to interface with the app

---
+++ src/services/location/location.context.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect, createContext } from "react";
+
+import { locationRequest, locationTransform } from "./location.service";
+
+export const LocationContext = createContext();
+
+export const LocationContextProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [keyword, setKeyword] = useState("san francisco");
+
+  const onSearch = (searchKeyword) => {
+    setIsLoading(true);
+    setKeyword(searchKeyword);
+    locationRequest(keyword)
+      .then(locationTransform)
+      .then((result) => {
+        setIsLoading(false);
+        setLocation(result);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err);
+      });
+  };
+
+  useEffect(() => {}, []);
+
+  return (
+    <LocationContext.Provider
+      value={{
+        isLoading,
+        error,
+        location,
+        search: onSearch,
+        keyword,
+      }}
+    >
+      {children}
+    </LocationContext.Provider>
+  );
+};
