a0b756e7ad7e23b7363ef3ac54d65d79410ffddf
CozyBrian <dragonbrian33@gmail.com>
2021-11-14 22:51:41 +0000

    Search Feature

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Searchbar } from "react-native-paper";
+import { LocationContext } from "../../../services/location/location.context";
 import { SearchContainer } from "./restaurant-info-card.styles";
 
 export const Search = () => {
+  const { keyword, search } = useContext(LocationContext);
+  const [searchKeyword, setSearchKeyword] = useState(keyword);
+
+  useEffect(() => {
+    setSearchKeyword(keyword);
+  }, [keyword]);
+
   return (
     <SearchContainer>
-      <Searchbar />
+      <Searchbar
+        placeholder="Search for a location"
+        value={searchKeyword}
+        onSubmitEditing={() => {
+          search(searchKeyword);
+        }}
+        onChangeText={(text) => {
+          if (!text.length) {
+            return;
+          }
+          setSearchKeyword(text);
+        }}
+      />
     </SearchContainer>
   );
 };
--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,16 +1,23 @@
-import React, { useState, createContext, useEffect, useMemo } from "react";
+import React, {
+  useState,
+  createContext,
+  useEffect,
+  useMemo,
+  useContext,
+} from "react";
 
 import {
   restaurantsRequest,
   restaurantsTransform,
 } from "./restaurants.service";
-
+import { LocationContext } from "../location/location.context";
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = ({ children }) => {
   const [restaurants, setRestaurants] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const { location } = useContext(LocationContext);
 
   const retriveRestaurants = () => {
     setIsLoading(true);
