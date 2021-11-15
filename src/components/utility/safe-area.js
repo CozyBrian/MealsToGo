9fa6d4356553346792d118258928fdf1d30a0d13
CozyBrian <dragonbrian33@gmail.com>
2021-11-15 03:14:56 +0000

    Fixed Margin bug on RestaurantDetailScreen

--- src/components/utility/safe-area.js
+++ src/components/utility/safe-area.js
@@ -5,3 +5,7 @@ export const SafeArea = styled.SafeAreaView`
   flex: 1;
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
+
+export const SafeAreaRDS = styled.SafeAreaView`
+  flex: 1;
+`;
