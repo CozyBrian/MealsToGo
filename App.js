1ca76acf0cff0e4f90d306c876c2a0921d473173
CozyBrian <dragonbrian33@gmail.com>
2021-11-15 02:17:59 +0000

    Moved all BottomNavigation to separate component

--- App.js
+++ App.js
@@ -1,72 +1,15 @@
 import React from "react";
-import { Text } from "react-native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { SafeArea } from "./src/components/utility/safe-area";
-import { Ionicons } from "@expo/vector-icons";
-
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
-
-import { RestaurantScreen } from "./src/features/restaurants/screens/restaurant.screen";
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
-
-const Tab = createBottomTabNavigator();
-
-const Settings = () => (
-  <SafeArea>
-    <Text>Settings</Text>
-  </SafeArea>
-);
-
-const Map = () => (
-  <SafeArea>
-    <Text>Map</Text>
-  </SafeArea>
-);
-
-const screenoption = ({ route }) => ({
-  tabBarIcon: ({ focused, color, size }) => {
-    let iconName;
-
-    switch (route.name) {
-      case "Restaurant":
-        iconName = focused ? "restaurant" : "restaurant-outline";
-        break;
-      case "Map":
-        iconName = focused ? "map" : "map-outline";
-        break;
-      case "Settings":
-        iconName = focused ? "settings-sharp" : "settings-outline";
-        break;
-
-      default:
-        break;
-    }
-
-    return <Ionicons name={iconName} size={size} color={color} />;
-  },
-  tabBarActiveTintColor: "tomato",
-  tabBarInactiveTintColor: "gray",
-  headerShown: false,
-});
-
-function MyTabs() {
-  return (
-    <Tab.Navigator screenOptions={screenoption}>
-      <Tab.Screen name="Restaurant" component={RestaurantScreen} />
-      <Tab.Screen name="Map" component={Map} />
-      <Tab.Screen name="Settings" component={Settings} />
-    </Tab.Navigator>
-  );
-}
+import { Navigation } from "./src/infrastructure/navigation";
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -86,9 +29,7 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <LocationContextProvider>
           <RestaurantsContextProvider>
-            <NavigationContainer>
-              <MyTabs />
-            </NavigationContainer>
+            <Navigation />
           </RestaurantsContextProvider>
         </LocationContextProvider>
       </ThemeProvider>
