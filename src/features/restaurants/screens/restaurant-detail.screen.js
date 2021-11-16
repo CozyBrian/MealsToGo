9c50d9f71e3a924a32c5bbc4ccf20a732835ab54
CozyBrian <dragonbrian33@gmail.com>
2021-11-16 04:08:23 +0000

    Fixed statusbar color on android

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { StatusBar } from "expo-status-bar";
 import { ScrollView } from "react-native-gesture-handler";
 import { List } from "react-native-paper";
 import { SafeAreaRDS } from "../../../components/utility/safe-area";
@@ -7,40 +8,45 @@ import { RestaurantInfoCard } from "../components/restaurant-info-card.component
 
 const MenuView = () => {
   return (
-    <ScrollView>
-      <List.Section title="MENU">
-        <List.Accordion
-          title="Breakfast"
-          left={(props) => <List.Icon {...props} icon="bread-slice-outline" />}
-        >
-          <List.Item title="Kooko with Fried Bean-Cake" />
-          <List.Item title="Iced Lipton Beverage with Sausage Sandwich" />
-        </List.Accordion>
-        <List.Accordion
-          title="Lunch"
-          left={(props) => <List.Icon {...props} icon="food" />}
-        >
-          <List.Item title="Assorted Fried-Rice with Fish" />
-          <List.Item title="Special Jollof with Fish" />
-        </List.Accordion>
-        <List.Accordion
-          title="Dinner"
-          left={(props) => (
-            <List.Icon {...props} icon="silverware-fork-knife" />
-          )}
-        >
-          <List.Item title="Banku with Fish and Okra Soup" />
-          <List.Item title="Fufu with Chicken and Groundnut Soup" />
-        </List.Accordion>
-        <List.Accordion
-          title="Drinks"
-          left={(props) => <List.Icon {...props} icon="cup" />}
-        >
-          <List.Item title="Sobolo" />
-          <List.Item title="Coka Cola" />
-        </List.Accordion>
-      </List.Section>
-    </ScrollView>
+    <>
+      <StatusBar style="light" />
+      <ScrollView>
+        <List.Section title="MENU">
+          <List.Accordion
+            title="Breakfast"
+            left={(props) => (
+              <List.Icon {...props} icon="bread-slice-outline" />
+            )}
+          >
+            <List.Item title="Kooko with Fried Bean-Cake" />
+            <List.Item title="Iced Lipton Beverage with Sausage Sandwich" />
+          </List.Accordion>
+          <List.Accordion
+            title="Lunch"
+            left={(props) => <List.Icon {...props} icon="food" />}
+          >
+            <List.Item title="Assorted Fried-Rice with Fish" />
+            <List.Item title="Special Jollof with Fish" />
+          </List.Accordion>
+          <List.Accordion
+            title="Dinner"
+            left={(props) => (
+              <List.Icon {...props} icon="silverware-fork-knife" />
+            )}
+          >
+            <List.Item title="Banku with Fish and Okra Soup" />
+            <List.Item title="Fufu with Chicken and Groundnut Soup" />
+          </List.Accordion>
+          <List.Accordion
+            title="Drinks"
+            left={(props) => <List.Icon {...props} icon="cup" />}
+          >
+            <List.Item title="Sobolo" />
+            <List.Item title="Coka Cola" />
+          </List.Accordion>
+        </List.Section>
+      </ScrollView>
+    </>
   );
 };
 
