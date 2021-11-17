0e2b6fec8e4963c12d4c5ab9b454218fba1624c1
CozyBrian <dragonbrian33@gmail.com>
2021-11-17 00:31:43 +0000

    added Favourites Icon component on Cards

---
+++ src/components/favourites/favourite.component.js
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { AntDesign } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+import { FavouritesContext } from "../../services/favourites/favourites.context";
+
+const FavouriteButton = styled(TouchableOpacity)`
+  background-color: transparent;
+  border-color: #20232a;
+  position: absolute;
+  top: 10px;
+  right: -30px;
+  width: 64px;
+  z-index: 9;
+`;
+
+export const Favourite = ({ restaurant }) => {
+  const { favourites, addToFavourites, removeFromFavourites } =
+    useContext(FavouritesContext);
+
+  const isFavourite = favourites.find((r) => r.placeId === restaurant.placeId);
+
+  return (
+    <FavouriteButton
+      onPress={() =>
+        !isFavourite
+          ? addToFavourites(restaurant)
+          : removeFromFavourites(restaurant)
+      }
+    >
+      <AntDesign
+        name={isFavourite ? "heart" : "hearto"}
+        size={24}
+        color={isFavourite ? "red" : "white"}
+      />
+    </FavouriteButton>
+  );
+};
